4a94cecf1a0770aedb048f8591baa0855f61d613
Emmanuel Herrero <emmanuelherrero@gmail.com>
2019-08-30 20:04:53 -0700

    Filter component scaffold

---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const Filters: FC = () => {
+	const foo = "bar";
+	console.log(foo);
+
+	return <form onSubmit={() => null}></form>;
+};
+
+export default Filters;
