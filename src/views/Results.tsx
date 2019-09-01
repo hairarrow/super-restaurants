62cf3bebea0af098ef3bd388c3859c8acfcaf35f
Emmanuel Herrero <emmanuelherrero@gmail.com>
2019-08-31 20:16:16 -0700

    Wrap results view with SearchContextProvider

--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -1,17 +1,28 @@
 import React, { FC } from "react";
 import Filters from "../components/Filters";
 import Header from "../components/Header";
+import { createSearchContext } from "../components/Search/SearchContext";
+import SearchReducer, {
+	initialState
+} from "../components/Search/SearchReducer";
+
+export const [Context, SearchContextProvider] = createSearchContext(
+	initialState,
+	SearchReducer
+);
 
 const ResultsView: FC = () => (
-	<main>
-		<Header title="Restaurants">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.
-			</p>
-		</Header>
-		<Filters />
-	</main>
+	<SearchContextProvider>
+		<main>
+			<Header title="Restaurants">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</p>
+			</Header>
+			<Filters />
+		</main>
+	</SearchContextProvider>
 );
 
 export default ResultsView;
