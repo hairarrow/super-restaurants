b11e75bdd4057c5c6168fbe04605c3ab8abcf443
Emmanuel Herrero <emmanuelherrero@gmail.com>
2019-09-05 16:50:19 -0700

    Update restaurant view with quick query implementation

--- src/views/Restaurant.tsx
+++ src/views/Restaurant.tsx
@@ -1,7 +1,17 @@
+import { useQuery } from "@apollo/react-hooks";
 import React, { FC } from "react";
+import { businessQuery } from "../graphql/queries/business";
+import { businessVariables } from "../graphql/queries/types/Business";
 
-const RestaurantView: FC<any> = ({ match }) => (
-	<h1>Restaurant: {match.params.id}</h1>
-);
+const RestaurantView: FC<any> = ({ match }) => {
+	const { loading, error, data } = useQuery<businessVariables>(businessQuery, {
+		variables: { id: match.params.id }
+	});
+
+	if (loading) return <h1>Loading</h1>;
+	if (error) return <h1 style={{ color: "#f00" }}>Error</h1>;
+
+	return <h1>{JSON.stringify(data)}</h1>;
+};
 
 export default RestaurantView;
