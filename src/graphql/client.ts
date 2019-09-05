6162dbfc73154910eb74ea178d173573a9abfa71
Emmanuel Herrero <emmanuelherrero@gmail.com>
2019-09-04 21:50:31 -0700

    update firebase address

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -4,7 +4,8 @@ const client = new ApolloClient({
 	// uri: "/super-restaurants/us-central1/api/graphql"
 	// Work around to make Storybook work
 	// TODO Make this an env variable
-	uri: "http://localhost:5000/super-restaurants/us-central1/api/graphql"
+	// uri: "http://localhost:5000/super-restaurants/us-central1/api/graphql"
+	uri: "https://us-central1-super-restaurants.cloudfunctions.net/api"
 });
 
 export default client;
